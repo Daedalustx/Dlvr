44e60dbefc713fcd61fbf05f8a99429e33f55119
Daedalustx <daedalus_me@mac.com>
2014-12-07 13:20:01 -0600

    makes column headers into custom directive

--- list-view/list.js
+++ list-view/list.js
@@ -8,50 +8,11 @@ listModule.config(['$routeProvider', function($routeProvider) {
     controller: 'VideoListCtrl'
   }).when('/deliverables/:groupName', {
     templateUrl: 'list-view/list.html',
-    controller: 'NestedVideoListCtrl'
+    controller: 'VideoListCtrl'
   });;
 }])
 
-.controller('VideoListCtrl', [ '$scope', '$http', function($scope, $http) {
-	$scope.getHeader = function(header) {
-		switch(header.index) {
-			case 0:
-				return "";
-			case 1:
-				return header.title;
-			case 2:
-			case 3:
-			case 4:
-				return header.display === true ? header.title : false;
-			default:
-				return;
-		}
-	};
-				
-	$scope.getIndex = function(index) {
-		if ($scope.data.colHeaders[0].displayConsecutive) {
-			return index + 1;
-		} else {
-			return $scope.data.videos[index].priority;
-		}
-	};
-}])
-.controller('NestedVideoListCtrl', [ '$scope', '$routeParams', function($scope, $routeParams) {
-	$scope.getHeader = function(header) {
-		switch(header.index) {
-			case 0:
-				return "";
-			case 1:
-				return $routeParams.groupName;
-			case 2:
-			case 3:
-			case 4:
-				return header.display === true ? header.title : false;
-			default:
-				return;
-		}
-	};
-				
+.controller('VideoListCtrl', [ '$scope', '$http', function($scope, $http) {				
 	$scope.getIndex = function(index) {
 		if ($scope.data.colHeaders[0].displayConsecutive) {
 			return index + 1;
@@ -61,7 +22,31 @@ listModule.config(['$routeProvider', function($routeProvider) {
 	};
 }]);
 
-listModule.directive('Headers', [ '$scope', function($scope) {
-	console.log($scope);
-}]);
+listModule.directive('dlvrHeaders', function() {
+	return {
+		restrict: 'E',
+		scope: {
+			myHeaders: '='
+		},
+		controller: function($scope, $routeParams) {
+			$scope.getHeader = function(header) {
+				var group = angular.isDefined($routeParams.groupName) ? $routeParams.groupName : null;
+				switch(header.index) {
+					case 0:
+						return header.display === true ? header.title : false;
+					case 1:
+						return header.title;
+					case 2:
+					case 3:
+					case 4:
+						return header.display === true ? header.title : false;
+					default:
+						return;
+				};
+			};
+		},
+		templateUrl: 'list-view/dlvr-headers.html'
+	};
+});
+
 
